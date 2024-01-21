2e48d8bcd3af120971d7545f7403fbc001ed4f8a
sophiabtran <150560518+sophiabtran@users.noreply.github.com>
2024-01-20 16:26:18 -0800

    map page stylling

--- frontend/src/pages/map.js
+++ frontend/src/pages/map.js
@@ -3,6 +3,9 @@ import "leaflet/dist/leaflet.css";
 import React from 'react';
 import '../styles/map.css'
 import {NavLink, useNavigate} from 'react-router-dom';
+import { useRef, useState } from 'react';
+// Import Swiper React components
+import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 function Map() {
     const navigate = useNavigate();
@@ -15,8 +18,30 @@ function Map() {
             />
     
         </MapContainer>
+        <div className='buttons'>
         <button className='logout' onClick={()=> navigate('../')}>Logout</button>
+        <button className='add-item' onClick={()=> navigate('../')}>+ Item</button>
+        <Element className="element" id="scroll-container" style={{
+            // position: 'relative',
+            overflow: 'scroll',
+            marginBottom: '2%'
+          }}>
+
+            <Element name="scroll-container-first-element" style={{
+              marginBottom: '2000px'
+            }}>
+              first element inside container
+          </Element>
+
+            <Element name="scroll-container-second-element" style={{
+              marginBottom: '200px'
+            }}>
+              second element inside container
+          </Element>
+          </Element>
         </div>
+        </div>
+        
     );
 }
 
