19cf4916cdae0541d6cd2ab2a666591eb12faa04
Tommy Chen <tchen175@ucsc.edu>
2024-01-20 22:58:54 -0800

    seperated the gen card function in its own file

--- frontend/src/pages/map.js
+++ frontend/src/pages/map.js
@@ -6,8 +6,8 @@ import { Icon } from "leaflet";
 // import MarkerClusterGroup from "react-leaflet-cluster";
 import '../styles/map.css'
 import {NavLink, useNavigate} from 'react-router-dom';
-
 import { useRef, useState, useEffect } from 'react';
+import GenCards from './generate_cards.js';
 
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
@@ -44,54 +44,6 @@ function LocationMarker() {
 }
 
 
-function GenCards() {
-    const [show,setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    let description = "Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae undecommodi aspernatur enim, consectetur. Cumque deleniti temporibus ipsam atque a dolores quisquam quisquam adipisci possimus laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia reiciendis porro quo magni incidunt dolore amet atque facilis ipsumdeleniti rem!";
-    let email = 'transophia4@gmail.com';
-    let name = 'airpods';
-    return (
-        <Element name="scroll-container-first-element" style={{
-            marginBottom: '2%'
-          }}><Card style={{ width: '100%', fontFamily:'Koulen', backgroundColor:'#E3E0E0', borderBlockColor:'white'}}>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary" onClick={handleShow}>
-            See More
-            </Button>
-            <Modal
-                show={show}
-                onHide={handleClose}
-                backdrop="static"
-                keyboard={false}
-                dialogClassName="modal-100w"
-                size="lg"
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>{name}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    {description}
-                </Modal.Body>
-                <Modal.Footer>
-                    <a className="btn btn-danger" href ={"mailto: "+ email}>Contact</a>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-                </Modal>
-          </Card.Body>
-        </Card>
-        </Element>
-
-    )
-
-}
 const data = [
   {
     latitude: 36,
