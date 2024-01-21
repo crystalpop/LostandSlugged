3a64d58f806865c34f29c2457976b69ad3f93f2e
Crystal Popeney <crystalpopeney@gmail.com>
2024-01-20 16:25:46 -0800

    Trying to add markers

--- frontend/src/pages/map.js
+++ frontend/src/pages/map.js
@@ -1,35 +1,119 @@
-import { MapContainer, TileLayer, Marker } from "react-leaflet"
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet"
 import "leaflet/dist/leaflet.css";
 import React from 'react';
 import { Icon } from "leaflet";
+// import MarkerClusterGroup from "react-leaflet-cluster";
+import { useState } from "react";
 
 import '../styles/map.css'
 
+
+
+const places = [
+    {
+        geocode: [36.9905, -122.0584],
+        popUp: "Item name"
+    },
+    {
+        geocode: [36.98407, -122.061606],
+        popUp: "Item name 2"
+    }
+
+];
+
+const customIcon = new Icon({
+    iconUrl: require("../styles/pin_icon.png"),
+    iconSize: [25, 38]
+});
+
+function LocationMarker() {
+    const [position, setPosition] = useState(null)
+
+    const map = useMapEvents({
+        click(e) {
+
+            setPosition(e.latlng);
+            places.push({geocode: [e.latlng.lat, e.latlng.lng], popUp: "Hello"});
+            console.log(places); // instead of this write it to database
+        },
+
+    });
+
+     // add position to an array called places
+
+    return position === null ? null : (
+        <Marker position={position} icon={customIcon}>
+            <Popup> ADD POPUP</Popup>
+        </Marker>
+    );
+
+}
+
+
+
+
+
 function Map() {
 
-    const markers = [
-        {
-            geocode: [36.9905, -122.0584],
-            popUp: "Test marker 1"
-        }
-    ];
+    
 
-    const customIcon = new Icon({
-        iconUrl: require("../styles/pin_icon.png"),
-        iconSize: [25, 38]
-    })
+    // const Markers = () => {
+    //     const map = useMapEvents({
+    //         click(e) {
+    //             setSelectedPosition([
+    //                 e.latlng.lat,
+    //                 e.latlng.lng
+    //             ]);
+    //         },
+    //     })
+
+    //     return (
+    //         selectedPosition ?
+    //         <Marker key={selectedPosition[0]}
+    //         position={selectedPosition}
+    //         interactive={false}
+    //         icon={customIcon} />
+    //         : null
+    //     );
+    // };
+        // {
+        //     geocode: [36.9905, -122.0584],
+        //     popUp: "Item name"
+        // },
+        // {
+        //     geocode: [36.98407, -122.061606],
+        //     popUp: "Item name 2"
+        // }
+    
+
+
+    // });
+
+    // const createCustomClusterIcon = (cluster) => {
+    //     return new divIcon({
+    //         html: '<div class="cluster-icon">$(cluster.getChildCount())</div>',
+    //         className: "custom-marker-cluster",
+    //         iconSize: point(33, 33, true)
+    //     });
+    // };
 
     return (
         <div>
         <MapContainer center={[36.9905, -122.0584]} zoom={15}>
-          <TileLayer
+
+
+
+           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap/org/copyright">OpenStreetMap</a> contributors'
             url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
 
-           {markers.map(marker => (
-            <Marker position={marker.geocode} icon={customIcon}></Marker>
-           ))}
+          {places.map(marker => (
+            <Marker position={marker.geocode} icon={customIcon}>
+            <Popup> ADD POPUP</Popup>
+            </Marker>
+          ))}
+          <LocationMarker />
     
         </MapContainer>
         </div>
